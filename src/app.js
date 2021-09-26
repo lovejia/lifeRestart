3db1f79ff52cca3e3e7a6eac4a09839fd8c1b562
piao <piao@qq.com>
2021-09-26 16:20:48 +0800

    增加更多的倍速播放选择，增加快速重新十连抽，增加快速开始修仙之路

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ class App{
         this.#life = new Life();
     }
 
+    #xiuXian = false;
     #life;
     #pages;
     #currentPage;
@@ -17,6 +18,7 @@ class App{
     #hintTimeout;
     #specialthanks;
     #autoTrajectory;
+    #speed = 0;
 
     async initial() {
         this.initPages();
@@ -65,7 +67,7 @@ class App{
                 <div style="font-size:1.5rem; font-weight:normal;">这垃圾人生一秒也不想呆了</div>
             </div>
             <button id="restart" class="mainbtn"><span class="iconfont">&#xe6a7;</span>立即重开</button>
-            <a id="discord" href="https://discord.gg/U3qrf49NMQ" style="z-index: 9999; display: none;" aria-label="Chat on Discord"><button class="discord-btn"><svg width="50%" height="55" viewBox="0 0 71 55" fill="none" xmlns="http://www.w3.org/2000/svg"><g clip-path="url(#clip0)"><path d="M60.1045 4.8978C55.5792 2.8214 50.7265 1.2916 45.6527 0.41542C45.5603 0.39851 45.468 0.440769 45.4204 0.525289C44.7963 1.6353 44.105 3.0834 43.6209 4.2216C38.1637 3.4046 32.7345 3.4046 27.3892 4.2216C26.905 3.0581 26.1886 1.6353 25.5617 0.525289C25.5141 0.443589 25.4218 0.40133 25.3294 0.41542C20.2584 1.2888 15.4057 2.8186 10.8776 4.8978C10.8384 4.9147 10.8048 4.9429 10.7825 4.9795C1.57795 18.7309 -0.943561 32.1443 0.293408 45.3914C0.299005 45.4562 0.335386 45.5182 0.385761 45.5576C6.45866 50.0174 12.3413 52.7249 18.1147 54.5195C18.2071 54.5477 18.305 54.5139 18.3638 54.4378C19.7295 52.5728 20.9469 50.6063 21.9907 48.5383C22.0523 48.4172 21.9935 48.2735 21.8676 48.2256C19.9366 47.4931 18.0979 46.6 16.3292 45.5858C16.1893 45.5041 16.1781 45.304 16.3068 45.2082C16.679 44.9293 17.0513 44.6391 17.4067 44.3461C17.471 44.2926 17.5606 44.2813 17.6362 44.3151C29.2558 49.6202 41.8354 49.6202 53.3179 44.3151C53.3935 44.2785 53.4831 44.2898 53.5502 44.3433C53.9057 44.6363 54.2779 44.9293 54.6529 45.2082C54.7816 45.304 54.7732 45.5041 54.6333 45.5858C52.8646 46.6197 51.0259 47.4931 49.0921 48.2228C48.9662 48.2707 48.9102 48.4172 48.9718 48.5383C50.038 50.6034 51.2554 52.5699 52.5959 54.435C52.6519 54.5139 52.7526 54.5477 52.845 54.5195C58.6464 52.7249 64.529 50.0174 70.6019 45.5576C70.6551 45.5182 70.6887 45.459 70.6943 45.3942C72.1747 30.0791 68.2147 16.7757 60.1968 4.9823C60.1772 4.9429 60.1437 4.9147 60.1045 4.8978ZM23.7259 37.3253C20.2276 37.3253 17.3451 34.1136 17.3451 30.1693C17.3451 26.225 20.1717 23.0133 23.7259 23.0133C27.308 23.0133 30.1626 26.2532 30.1066 30.1693C30.1066 34.1136 27.28 37.3253 23.7259 37.3253ZM47.3178 37.3253C43.8196 37.3253 40.9371 34.1136 40.9371 30.1693C40.9371 26.225 43.7636 23.0133 47.3178 23.0133C50.9 23.0133 53.7545 26.2532 53.6986 30.1693C53.6986 34.1136 50.9 37.3253 47.3178 37.3253Z" fill="#ffffff"/></g><defs><clipPath id="clip0"><rect width="71" height="55" fill="white"/></clipPath></defs></svg>CHAT</button><style>.discord-btn {position: fixed;bottom: 0.5rem;left: 0.5rem;background-color: #5865F2;padding: 0.7rem;height: auto;color: white;text-align: right;vertical-align: middle;border: none;width: 6.5rem;font-size: 1rem;border-radius: 4px;}.discord-btn svg {height: 1.5rem;position: absolute;top: 50%;left: 0;transform: translateY(-50%);}.discord-btn:hover svg{animation:discord-wave 560ms ease-in-out;}@keyframes discord-wave{0%,100%{transform:translateY(-50%) rotate(0)}20%,60%{transform:translateY(-50%) rotate(-25deg)}40%,80%{transform:translateY(-50%) rotate(10deg)}}@media (max-width:500px){.discord-btn:hover svg{animation:none}.discord-btn svg{animation:discord-wave 560ms ease-in-out}}</style></a>
+            <a id="discord" href="https://discord.gg/U3qrf49NMQ" style="z-index: 9999;" aria-label="Chat on Discord"><button class="discord-btn"><svg width="50%" height="55" viewBox="0 0 71 55" fill="none" xmlns="http://www.w3.org/2000/svg"><g clip-path="url(#clip0)"><path d="M60.1045 4.8978C55.5792 2.8214 50.7265 1.2916 45.6527 0.41542C45.5603 0.39851 45.468 0.440769 45.4204 0.525289C44.7963 1.6353 44.105 3.0834 43.6209 4.2216C38.1637 3.4046 32.7345 3.4046 27.3892 4.2216C26.905 3.0581 26.1886 1.6353 25.5617 0.525289C25.5141 0.443589 25.4218 0.40133 25.3294 0.41542C20.2584 1.2888 15.4057 2.8186 10.8776 4.8978C10.8384 4.9147 10.8048 4.9429 10.7825 4.9795C1.57795 18.7309 -0.943561 32.1443 0.293408 45.3914C0.299005 45.4562 0.335386 45.5182 0.385761 45.5576C6.45866 50.0174 12.3413 52.7249 18.1147 54.5195C18.2071 54.5477 18.305 54.5139 18.3638 54.4378C19.7295 52.5728 20.9469 50.6063 21.9907 48.5383C22.0523 48.4172 21.9935 48.2735 21.8676 48.2256C19.9366 47.4931 18.0979 46.6 16.3292 45.5858C16.1893 45.5041 16.1781 45.304 16.3068 45.2082C16.679 44.9293 17.0513 44.6391 17.4067 44.3461C17.471 44.2926 17.5606 44.2813 17.6362 44.3151C29.2558 49.6202 41.8354 49.6202 53.3179 44.3151C53.3935 44.2785 53.4831 44.2898 53.5502 44.3433C53.9057 44.6363 54.2779 44.9293 54.6529 45.2082C54.7816 45.304 54.7732 45.5041 54.6333 45.5858C52.8646 46.6197 51.0259 47.4931 49.0921 48.2228C48.9662 48.2707 48.9102 48.4172 48.9718 48.5383C50.038 50.6034 51.2554 52.5699 52.5959 54.435C52.6519 54.5139 52.7526 54.5477 52.845 54.5195C58.6464 52.7249 64.529 50.0174 70.6019 45.5576C70.6551 45.5182 70.6887 45.459 70.6943 45.3942C72.1747 30.0791 68.2147 16.7757 60.1968 4.9823C60.1772 4.9429 60.1437 4.9147 60.1045 4.8978ZM23.7259 37.3253C20.2276 37.3253 17.3451 34.1136 17.3451 30.1693C17.3451 26.225 20.1717 23.0133 23.7259 23.0133C27.308 23.0133 30.1626 26.2532 30.1066 30.1693C30.1066 34.1136 27.28 37.3253 23.7259 37.3253ZM47.3178 37.3253C43.8196 37.3253 40.9371 34.1136 40.9371 30.1693C40.9371 26.225 43.7636 23.0133 47.3178 23.0133C50.9 23.0133 53.7545 26.2532 53.6986 30.1693C53.6986 34.1136 50.9 37.3253 47.3178 37.3253Z" fill="#ffffff"/></g><defs><clipPath id="clip0"><rect width="71" height="55" fill="white"/></clipPath></defs></svg>CHAT</button><style>.discord-btn {position: fixed;bottom: 0.5rem;left: 0.5rem;background-color: #5865F2;padding: 0.7rem;height: auto;color: white;text-align: right;vertical-align: middle;border: none;width: 6.5rem;font-size: 1rem;border-radius: 4px;}.discord-btn svg {height: 1.5rem;position: absolute;top: 50%;left: 0;transform: translateY(-50%);}.discord-btn:hover svg{animation:discord-wave 560ms ease-in-out;}@keyframes discord-wave{0%,100%{transform:translateY(-50%) rotate(0)}20%,60%{transform:translateY(-50%) rotate(-25deg)}40%,80%{transform:translateY(-50%) rotate(10deg)}}@media (max-width:500px){.discord-btn:hover svg{animation:none}.discord-btn svg{animation:discord-wave 560ms ease-in-out}}</style></a>
         </div>
         `);
 
@@ -195,6 +197,7 @@ class App{
             <div class="head" style="font-size: 1.6rem">天赋抽卡</div>
             <button id="random" class="mainbtn" style="position: fixed; top: 50%; left: 50%; transform: translate(-50%, -50%);"">10连抽！</button>
             <ul id="talents" class="selectlist"></ul>
+            <button id="change" class="mainbtn">重新10连抽</button>
             <button id="next" class="mainbtn">请选择3个</button>
         </div>
         `);
@@ -203,50 +206,61 @@ class App{
             return $(`<li class="grade${grade}b">${name}（${description}）</li>`)
         };
 
+        const appendTalent = (talent) => {
+            this.#xiuXian = this.#xiuXian || (talent.id==1048);
+            const li = createTalent(talent);
+            talentPage.find('#talents').append(li);
+            li.click(()=>{
+                if(li.hasClass('selected')) {
+                    li.removeClass('selected')
+                    this.#talentSelected.delete(talent);
+                    if(this.#talentSelected.size<3) {
+                        talentPage.find('#change').text('重新10连抽')
+                        talentPage.find('#next').text('请选择3个')
+                    }
+                } else {
+                    if(this.#talentSelected.size==3) {
+                        this.hint('只能选3个天赋');
+                        return;
+                    }
+
+                    const exclusive = this.#life.exclusive(
+                        Array.from(this.#talentSelected).map(({id})=>id),
+                        talent.id
+                    );
+                    if(exclusive != null) {
+                        for(const { name, id } of this.#talentSelected) {
+                            if(id == exclusive) {
+                                this.hint(`与已选择的天赋【${name}】冲突`);
+                                return;
+                            }
+                        }
+                        return;
+                    }
+                    li.addClass('selected');
+                    this.#talentSelected.add(talent);
+                    if(this.#talentSelected.size==3) {
+                        talentPage.find('#change').text('昆仑山修士')
+                        talentPage.find('#next').text('开始新人生')
+                    }
+                }
+            });
+        }
+
         talentPage
             .find('#random')
             .click(()=>{
                 talentPage.find('#random').hide();
-                const ul = talentPage.find('#talents');
+                this.#xiuXian = false;
+                this.#talentSelected.clear();
+                talentPage.find('#change').text('重新10连抽')
+                talentPage.find('#next').text('请选择3个')
                 this.#life.talentRandom()
                     .forEach(talent=>{
-                        const li = createTalent(talent);
-                        ul.append(li);
-                        li.click(()=>{
-                            if(li.hasClass('selected')) {
-                                li.removeClass('selected')
-                                this.#talentSelected.delete(talent);
-                                if(this.#talentSelected.size<3) {
-                                    talentPage.find('#next').text('请选择3个')
-                                }
-                            } else {
-                                if(this.#talentSelected.size==3) {
-                                    this.hint('只能选3个天赋');
-                                    return;
-                                }
-
-                                const exclusive = this.#life.exclusive(
-                                    Array.from(this.#talentSelected).map(({id})=>id),
-                                    talent.id
-                                );
-                                if(exclusive != null) {
-                                    for(const { name, id } of this.#talentSelected) {
-                                        if(id == exclusive) {
-                                            this.hint(`与已选择的天赋【${name}】冲突`);
-                                            return;
-                                        }
-                                    }
-                                    return;
-                                }
-                                li.addClass('selected');
-                                this.#talentSelected.add(talent);
-                                if(this.#talentSelected.size==3) {
-                                    talentPage.find('#next').text('开始新人生')
-                                }
-                            }
-                        });
+                        appendTalent(talent);
                     });
-                talentPage.find('#next').show()
+                talentPage.find('#next').show();
+                (this.#life.getTotal().times > 49) && talentPage.find('#change').show();
             });
 
         talentPage
@@ -257,8 +271,25 @@ class App{
                     return;
                 }
                 talentPage.find('#next').hide()
+                talentPage.find('#change').hide()
                 this.#totalMax = 20 + this.#life.getTalentAllocationAddition(Array.from(this.#talentSelected).map(({id})=>id));
                 this.switch('property');
+            })
+
+        talentPage
+            .find('#change')
+            .click(()=>{
+                if(this.#talentSelected.size!=3) {
+                    talentPage.find('#talents').children().remove();
+                    talentPage.find('#random').click();
+                    return;
+                }
+                if (this.#xiuXian) {
+                    this.hint('选择神秘的小盒子有机会开启修仙人生');
+                }else{
+                    appendTalent({grade: 3, name: '神秘的小盒子', description: '100岁时才能开启', id: 1048});
+                    this.#xiuXian = true
+                }
             })
 
         // Property
@@ -406,7 +437,7 @@ class App{
             <ul id="lifeTrajectory" class="lifeTrajectory"></ul>
             <div class="btn-area">
                 <button id="auto" class="mainbtn">自动播放</button>
-                <button id="auto2x" class="mainbtn">自动播放2x</button>
+                <button id="auto2x" class="mainbtn">一眼万年</button>
                 <button id="summary" class="mainbtn">人生总结</button>
                 <button id="domToImage" class="mainbtn">人生回放</button>
             </div>
@@ -439,6 +470,7 @@ class App{
                 if(isEnd) {
                     $(document).unbind("keydown");
                     this.#isEnd = true;
+                    this.#speed = 0;
                     trajectoryPage.find('#summary').show();
                     trajectoryPage.find('#auto').hide();
                     trajectoryPage.find('#auto2x').hide();
@@ -483,11 +515,12 @@ class App{
                 this.switch('summary');
             });
 
-        const auto = tick=>{
+        const auto = ()=>{
             if(this.#autoTrajectory) {
                 clearInterval(this.#autoTrajectory);
                 this.#autoTrajectory = null;
-            } else {
+                auto();
+            } else if (this.#speed > 0){
                 if(!this.isEnd)
                     trajectoryPage
                         .find('#lifeTrajectory')
@@ -501,16 +534,19 @@ class App{
                             .find('#lifeTrajectory')
                             .click();
                     }
-                }, tick);
+                }, 1000 / this.#speed);
+                trajectoryPage.find('#auto').text("自动播放"+(this.#speed < 10?(this.#speed+'x'):""));
+            }else{
+                trajectoryPage.find('#auto').text("自动播放");
             }
         };
 
         trajectoryPage
             .find('#auto')
-            .click(()=>auto(1000));
+            .click(()=>{this.#speed = (this.#speed + 1) % 6;auto();});
         trajectoryPage
             .find('#auto2x')
-            .click(()=>auto(500));
+            .click(()=>{this.#speed = 50;auto();});
 
         // Summary
         const summaryPage = $(`
